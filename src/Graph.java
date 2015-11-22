be2500d67c1fbaa7f35493aa4aa19ede9f22cac5
Jason Safaiyeh <jsafaiyeh@gmail.com>
2015-11-22 11:06:36 -0800

    Created data structure without algorithm implementation

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+
 /**
- * Created by JSafaiyeh on 11/22/15.
+ * Represents a graph data structure.
  */
 public class Graph {
+
+    ArrayList<Node> vertices;
+
+    public Graph() {
+        vertices = new ArrayList<>();
+    }
+
+    public void addVertice(Node node) {
+        vertices.add(node);
+    }
+
+    /**
+     * Algorithms to calculate shortest path
+     */
+    public void getPathBellmanFord() {
+
+    }
+
+    public void getPathDijkstrasWithList() {
+
+    }
+
+    public void getPathDijkstrasWithModifiedBinaryHeap() {
+
+    }
+
+    public void getPathDijkstrasWithModifiedFibonacciHeap() {
+
+    }
+
+    public void getPathGabows() {
+
+    }
 }
